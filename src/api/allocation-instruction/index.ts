1db1314bc2a7181e8a9c36d46a58ddd0818e4b4c
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-02-03 15:07:33 +0100

    feat: prepare api skeletons for allocation

--- src/api/allocation-instruction/index.ts
+++ src/api/allocation-instruction/index.ts
@@ -1,1 +1,30 @@
 import * as openapi from '@openapi-ts/allocation-instruction-v1';
+
+import { OpenAPIBackend, type Context } from 'openapi-backend';
+import definition from '@token-standard/splice-api-token-allocation-instruction-v1/openapi/allocation-instruction-v1.yaml';
+import { notFound } from '../error';
+import admin from 'src/util/admin';
+
+const api = new OpenAPIBackend({
+  definition,
+  quick: true,
+});
+
+const getAllocationFactory = async (): Promise<openapi.FactoryWithChoiceContext> => {
+  return {
+    factoryId: '',
+    choiceContext: {
+      choiceContextData: {},
+      disclosedContracts: [],
+    },
+  };
+};
+
+api.register({
+  getAllocationFactory,
+  notFound: () => notFound,
+});
+
+await api.init();
+
+export default api;
--- src/api/allocation/index.ts
+++ src/api/allocation/index.ts
@@ -1,1 +1,43 @@
 import * as openapi from '@openapi-ts/allocation-v1';
+
+import { OpenAPIBackend, type Context } from 'openapi-backend';
+import definition from '@token-standard/splice-api-token-allocation-v1/openapi/allocation-v1.yaml';
+import { notFound } from '../error';
+import admin from 'src/util/admin';
+
+const api = new OpenAPIBackend({
+  definition,
+  quick: true,
+});
+
+const getAllocationTransferContext = async (): Promise<openapi.ChoiceContext> => {
+  return {
+    choiceContextData: {},
+    disclosedContracts: [],
+  };
+};
+
+const getAllocationWithdrawContext = (): openapi.ChoiceContext => {
+  return {
+    choiceContextData: {},
+    disclosedContracts: [],
+  };
+};
+
+const getAllocationCancelContext = (): openapi.ChoiceContext => {
+  return {
+    choiceContextData: {},
+    disclosedContracts: [],
+  };
+};
+
+api.register({
+  getAllocationTransferContext,
+  getAllocationWithdrawContext,
+  getAllocationCancelContext,
+  notFound: () => notFound,
+});
+
+await api.init();
+
+export default api;
